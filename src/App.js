cadfab55323faec8e16eee59c1bf5593a3f87c5d
sookkiong <solnaamu98@gmail.com>
2022-07-20 21:52:36 +0900

    update: nav list 수정

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import {Navbar, Container, Nav} from 'react-bootstrap';
 function App() {
   return (
     <div className="App">
-       <Navbar variant="white" className="redBg">
+       <Navbar variant="dark" className="redBg">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home">Bootstrap</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#home">메뉴1</Nav.Link>
+            <Nav.Link href="#features">메뉴2</Nav.Link>
+            <Nav.Link href="#pricing">메뉴3</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
