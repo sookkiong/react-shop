09cc16f461ce34c543bb23fde5efcf4e6921632b
sookkiong <solnaamu98@gmail.com>
2022-07-22 17:51:37 +0900

    feat: 이미지 넣는 법, 경로 설정

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Navbar, Container, Nav} from 'react-bootstrap';
+import {Navbar, Container, Nav, Row, Col} from 'react-bootstrap';
+
 
 function App() {
   return (
     <div className="App">
-       <Navbar variant="dark" className="redBg">
+       <Navbar bg="light" variant="light">
         <Container>
-          <Navbar.Brand href="#home">Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">Soo Shop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">메뉴1</Nav.Link>
-            <Nav.Link href="#features">메뉴2</Nav.Link>
-            <Nav.Link href="#pricing">메뉴3</Nav.Link>
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
+            <Nav.Link href="#pricing">My</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
+
+      <div className='main-bg'></div>
+
+      <Container>
+      <Row>
+        <Col sm>
+          <img src={`${process.env.PUBLIC_URL}/img/cn1.jpg`} width="100%"/>
+          <h4>상품명</h4>
+          <p>상품설명</p>
+        </Col>
+        <Col sm>
+          <img src="img/cn2.jpg" width="100%"/>
+          <h4>상품명</h4>
+          <p>상품설명</p>
+        </Col>
+        <Col sm>
+          <img src="img/cn3.jpg" width="100%"/>
+          <h4>상품명</h4>
+          <p>상품설명</p>
+        </Col>
+      </Row>
+    </Container>
+
     </div>
   );
 }
