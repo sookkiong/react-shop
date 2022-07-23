39c621c88b05931aa27ec6ce8baa7600fef5f5ce
sookkiong <solnaamu98@gmail.com>
2022-07-23 17:40:17 +0900

    feat: 상품목록 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import data from './data.js';
 function App() {
 
   let [shoes] = useState(data)
-
+  const goodsArr = [1,2,3];
 
   return (
     <div className="App">
@@ -25,28 +25,52 @@ function App() {
 
       <div className='main-bg'></div>
 
-      <Container>
-      <Row>
-        <Col sm>
-          <img src={`${process.env.PUBLIC_URL}/img/cn1.jpg`} width="80%"/>
-          <h4>{shoes[0].title}</h4>
-          <p>{shoes[0].price}</p>
-        </Col>
-        <Col sm>
-          <img src="img/cn2.jpg" width="80%"/>
-          <h4>{shoes[1].title}</h4>
-          <p>{shoes[1].price}</p>
-        </Col>
-        <Col sm>
-          <img src="img/cn3.jpg" width="80%"/>
-          <h4>{shoes[2].title}</h4>
-          <p>{shoes[2].price}</p>
-        </Col>
-      </Row>
-    </Container>
+      {/* <Container>
+        <Row>
+          <Col sm>
+            <img src={`${process.env.PUBLIC_URL}/img/cn1.jpg`} width="80%"/>
+            <h4>{shoes[0].title}</h4>
+            <p>{shoes[0].price}</p>
+          </Col>
+          <Col sm>
+            <img src="img/cn2.jpg" width="80%"/>
+            <h4>{shoes[1].title}</h4>
+            <p>{shoes[1].price}</p>
+          </Col>
+          <Col sm>
+            <img src="img/cn3.jpg" width="80%"/>
+            <h4>{shoes[2].title}</h4>
+            <p>{shoes[2].price}</p>
+          </Col>
+        </Row>
+      </Container> */}
 
+      <Goods shoes={shoes} goodsArr={goodsArr}/>
     </div>
   );
+}
+
+const Goods = (props) => {
+  return(
+    <div>
+      <Row>
+        {
+          props.shoes.map((v, i)=>{
+            return(
+              <div>
+                <Col sm>
+                <img src={`${process.env.PUBLIC_URL}/img/cn${i+1}.jpg`} width="80%" alt='사진1'/>
+                <h4>{v.title}</h4>
+                <p>{v.price}</p>
+                </Col>
+              </div>
+            )
+              
+          })
+        }
+      </Row>
+    </div>
+  )
 }
 
 export default App;
