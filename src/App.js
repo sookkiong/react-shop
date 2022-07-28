2fefe52f3c1162bd0e1770afe195b324aa7ba128
sookkiong <solnaamu98@gmail.com>
2022-07-28 20:30:53 +0900

    feat: 라우터를 이용해 페이지 이동

--- src/App.js
+++ src/App.js
@@ -1,75 +1,62 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Navbar, Container, Nav, Row, Col} from 'react-bootstrap';
-import {useState} from "react";
+import { Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
+import { useState } from 'react';
 import data from './data.js';
-
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
-
-  let [shoes] = useState(data)
+  let [shoes] = useState(data);
 
   return (
     <div className="App">
-       <Navbar bg="light" variant="light">
+      <Navbar bg="light" variant="light">
         <Container>
           <Navbar.Brand href="#home">Soo Shop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Cart</Nav.Link>
-            <Nav.Link href="#pricing">My</Nav.Link>
+            <Link to="/" style={{ paddingRight: '15px' }}>
+              홈
+            </Link>
+            <Link to="/detail">상세페이지</Link>
           </Nav>
         </Container>
       </Navbar>
 
-      <div className='main-bg'></div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <div className="main-bg"></div>
 
-      {/* <Container>
-        <Row>
-          <Col sm>
-            <img src={`${process.env.PUBLIC_URL}/img/cn1.jpg`} width="80%"/>
-            <h4>{shoes[0].title}</h4>
-            <p>{shoes[0].price}</p>
-          </Col>
-          <Col sm>
-            <img src="img/cn2.jpg" width="80%"/>
-            <h4>{shoes[1].title}</h4>
-            <p>{shoes[1].price}</p>
-          </Col>
-          <Col sm>
-            <img src="img/cn3.jpg" width="80%"/>
-            <h4>{shoes[2].title}</h4>
-            <p>{shoes[2].price}</p>
-          </Col>
-        </Row>
-      </Container> */}
-
-      <Goods shoes={shoes}/>
+              <Goods shoes={shoes} />
+            </div>
+          }
+        />
+        <Route path="/detail" element={<div>상세페이지임</div>} />
+      </Routes>
     </div>
   );
 }
 
 const Goods = (props) => {
-  return(
+  return (
     <div>
       <Row>
-        {
-          props.shoes.map((v, i)=>{
-            return(
-              <div>
-                <Col sm>
-                <img src={`${process.env.PUBLIC_URL}/img/cn${i+1}.jpg`} width="80%" alt='사진1'/>
+        {props.shoes.map((v, i) => {
+          return (
+            <div>
+              <Col sm>
+                <img src={`${process.env.PUBLIC_URL}/img/cn${i + 1}.jpg`} width="80%" alt="사진1" />
                 <h4>{v.title}</h4>
                 <p>{v.price}</p>
-                </Col>
-              </div>
-            )
-              
-          })
-        }
+              </Col>
+            </div>
+          );
+        })}
       </Row>
     </div>
-  )
-}
+  );
+};
 
 export default App;
