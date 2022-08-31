4941cee926b45a52a3f1c4cb6cd4e71745aa43c3
sookkiong <solnaamu98@gmail.com>
2022-08-31 21:02:28 +0900

    feat: redux store에 있던 state 가져다쓰기

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,6 +1,12 @@
 import './../App.css';
+import { useSelector } from 'react-redux';
 
 function Cart() {
+  let a = useSelector((state) => {
+    return state; //이 부분의 state는 store.js에서 가져온 state들을 말한다. state.stock 가능
+  });
+  console.log(a.stock);
+
   return (
     <div>
       <table>
@@ -13,12 +19,15 @@ function Cart() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>안녕</td>
-            <td>안녕</td>
-            <td>안녕</td>
-          </tr>
+          {a.list.map((v, i) => {
+            return (
+              <tr>
+                <td>{v.id}</td>
+                <td>{v.name}</td>
+                <td>{v.count}</td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
